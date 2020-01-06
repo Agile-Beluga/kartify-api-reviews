e26c1a754e361c454e3efecef0f33eb47ee0aaca
Alec Champaign <alecchampaign@protonmail.com>
2020-01-06 10:17:23 -0500

    Move files

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const parser = require('body-parser');
-
-const PORT = process.env.port || 3000;
-
-app.use(parser.json());
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
